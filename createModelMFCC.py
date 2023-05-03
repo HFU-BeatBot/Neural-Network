eb01e646b15d65c55d8d854e6d76416b7efdb0f4
Domi <Dominik.Suhm@gmx.de>
2023-05-03 16:05:03 +0200

    Dropout + extra Layer

--- createModelMFCC.py
+++ createModelMFCC.py
@@ -2,10 +2,11 @@ import librosa
 import pandas as pd
 import numpy as np
 import tensorflow as tf
-# import matplotlib.pyplot as plt
 # '%matplotlib inline
 import os
 import csv
+
+from matplotlib import pyplot as plt
 # Preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
@@ -33,16 +34,18 @@ scaler = StandardScaler()
 X = scaler.fit_transform(np.array(data.iloc[:, :-1], dtype=float))
 
 # spliting of dataset into train and test dataset
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 
 # creating a model
 model = models.Sequential()
-model.add(layers.Dense(256, activation='relu', input_shape=(X_train.shape[1],)))
-
+model.add(layers.Dense(1028, activation='relu', input_shape=(X_train.shape[1],)))
+layers.Dropout(rate=0.3),  # apply 30% dropout to the next layer
+model.add(layers.Dense(512, activation='relu'))
+layers.Dropout(rate=0.3),
+model.add(layers.Dense(256, activation='relu'))
+layers.Dropout(rate=0.3),
 model.add(layers.Dense(128, activation='relu'))
-
-model.add(layers.Dense(64, activation='relu'))
-
+layers.Dropout(rate=0.3),
 model.add(layers.Dense(10, activation='softmax'))
 
 model.compile(optimizer='adam',
@@ -67,6 +70,15 @@ test_loss, test_acc = model.evaluate(X_test, y_test)
 # testen !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 # exportModel = model.save()
 print('test_acc: ', test_acc)
+
+#show loss + val_loss
+history_df = pd.DataFrame(history.history)
+history_df.loc[0:, ['loss', 'val_loss']].plot()
+plt.xlabel("epochs")
+plt.ylabel("loss")
+plt.title("loss + val_loss")
+plt.savefig('loss_val_loss_with_earlystopping.png')
+plt.show()
 
 # predictions
 predictions = model.predict(X_test)
