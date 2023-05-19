d15b8ba31dc3ec13c6d432b1e2254703eb6cb633
Dominik <Dominik.Suhm@gmx.de>
2023-05-19 14:27:11 +0200

    saved merged dataframe as csv

--- FMA/FMADataFilterTest.py
+++ FMA/FMADataFilterTest.py
@@ -16,7 +16,7 @@ column_headers_tracks = list(tracks_data.columns.values)
 
 # Change column names + drop first 2 rows
 tracks_data.columns = ['_'.join(col) for col in tracks_data.columns.values]  # Merge Headers
-tracksColumnsList = ['track_id', 'genre_top']
+tracksColumnsList = ['track_id', 'label']
 tracks_data.columns = tracksColumnsList  # Rename Column Header
 print("Tracks dataframe: ", "\n")
 print(tracks_data, "\n")
@@ -24,21 +24,22 @@ print(tracks_data, "\n")
 # Filter out rows without explicit genre
 genres = ['Electronic', 'Rock', 'Instrumental', 'Pop', 'Folk', 'Hip-Hop', 'International', 'Jazz'
           'Classical', 'Country', 'Spoken', 'Blues', 'Soul-RnB']
-filtered_tracks_data = tracks_data[tracks_data['genre_top'].isin(genres)]
+filtered_tracks_data = tracks_data[tracks_data['label'].isin(genres)]
 print("Filtered Tracks dataframe: ", "\n")
 print(filtered_tracks_data, "\n")
 
 #  Es gibt viele tracks mit mehreren Genre in tracks.csv
 #  Waere jetzt die Frage ob wir die rausfiltern oder was wir mit denen machen
 
-# Overview of top_level genres in genres.csv
+# top_level genres + Metal variations in genres.csv
 genres_extended = ['Electronic', 'Rock', 'Instrumental', 'Pop', 'Folk', 'Hip-Hop', 'International',
                    'Jazz', 'Classical', 'Country', 'Spoken', 'Blues', 'Soul-RnB', 'Metal',
                    'Death-Metal', 'Black-Metal', 'Country', 'Disco', 'Reggae - Dub']
 genres_data = pd.read_csv(genres_file, low_memory=False)
 genres_data = genres_data[genres_data['title'].isin(genres_extended)]
-genres_data = genres_data.sort_values(by=['#tracks'], ascending=True)  # sort by number of tracks
-#  print(genres_data)
+genres_data = genres_data.sort_values(by=['#tracks'])  # sort by number of tracks
+print("Genres dataframe: ", "\n")
+print(genres_data, "\n")
 
 #  Metal ist in der top_level category Rock
 #  Disco nur 300 Songs
@@ -52,18 +53,29 @@ features_data = features_data.drop(features_data.iloc[:, 41:168], axis=1)  # Onl
 
 # Reformat Header to single Index
 features_data.columns = ['_'.join(col) for col in features_data.columns.values]  # Merge Headers
-columnsList = ['track_id', 'mfcc1_mean', 'mfcc2_mean', 'mfcc3_mean', 'mfcc4_mean', 'mfcc5_mean', 'mfcc6_mean',
-               'mfcc7_mean', 'mfcc8_mean', 'mfcc9_mean', 'mfcc10_mean', 'mfcc11_mean', 'mfcc12_mean', 'mfcc13_mean',
-               'mfcc14_mean', 'mfcc15_mean', 'mfcc16_mean', 'mfcc17_mean', 'mfcc18_mean', 'mfcc19_mean', 'mfcc20_mean',
-               'mfcc1_std', 'mfcc2_std', 'mfcc3_std', 'mfcc4_std', 'mfcc5_std', 'mfcc6_std', 'mfcc7_std', 'mfcc8_std',
-               'mfcc9_std', 'mfcc10_std', 'mfcc11_std', 'mfcc12_std', 'mfcc13_std', 'mfcc14_std', 'mfcc15_std',
-               'mfcc16_std', 'mfcc17_std', 'mfcc18_std', 'mfcc19_std', 'mfcc20_std']
-features_data.columns = columnsList  # Rename Column Header
+columns = ['track_id', 'mfcc1_mean', 'mfcc2_mean', 'mfcc3_mean', 'mfcc4_mean', 'mfcc5_mean', 'mfcc6_mean',
+           'mfcc7_mean', 'mfcc8_mean', 'mfcc9_mean', 'mfcc10_mean', 'mfcc11_mean', 'mfcc12_mean', 'mfcc13_mean',
+           'mfcc14_mean', 'mfcc15_mean', 'mfcc16_mean', 'mfcc17_mean', 'mfcc18_mean', 'mfcc19_mean', 'mfcc20_mean',
+           'mfcc1_std', 'mfcc2_std', 'mfcc3_std', 'mfcc4_std', 'mfcc5_std', 'mfcc6_std', 'mfcc7_std', 'mfcc8_std',
+           'mfcc9_std', 'mfcc10_std', 'mfcc11_std', 'mfcc12_std', 'mfcc13_std', 'mfcc14_std', 'mfcc15_std',
+           'mfcc16_std', 'mfcc17_std', 'mfcc18_std', 'mfcc19_std', 'mfcc20_std']
+features_data.columns = columns  # Rename Column Header
 print("Filtered Features dataframe: ", "\n")
 print(features_data, "\n")
 
 # Merge tracks_data and features_data
 mergedData = pd.concat([features_data, tracks_data], axis=1, join='inner')  # Merge Data Frames
 mergedData = mergedData.drop(mergedData.columns[41], axis=1)  # Drop duplicate track_id
+rearrangedColumns = ['mfcc1_mean', 'mfcc1_std', 'mfcc2_mean', 'mfcc2_std', 'mfcc3_mean', 'mfcc3_std',
+                       'mfcc4_mean', 'mfcc4_std', 'mfcc5_std', 'mfcc5_mean', 'mfcc6_mean', 'mfcc6_std', 'mfcc7_mean',
+                       'mfcc7_std', 'mfcc8_mean', 'mfcc8_std', 'mfcc9_mean', 'mfcc9_std', 'mfcc10_mean', 'mfcc10_std',
+                       'mfcc11_mean', 'mfcc11_std', 'mfcc12_mean', 'mfcc12_std', 'mfcc13_mean', 'mfcc13_std',
+                       'mfcc14_mean', 'mfcc14_std', 'mfcc15_mean', 'mfcc15_std', 'mfcc16_mean', 'mfcc16_std',
+                       'mfcc17_mean', 'mfcc17_std', 'mfcc18_mean', 'mfcc18_std', 'mfcc19_mean', 'mfcc19_std',
+                       'mfcc20_mean', 'mfcc20_std', 'label']
+mergedData = mergedData[rearrangedColumns]  # Rearrange Columns
 print("Merged dataframe: ", "\n")
-print(mergedData, "\n")
+print(mergedData)
+
+# Writing merged dataframe to .csv
+mergedData.to_csv('mergedFMAData.csv', index=None)  # Write to csv without line numbering
