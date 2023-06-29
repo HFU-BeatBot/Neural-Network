e1451153a52de25698e351452d3a53a04da89840
suhmdomi <dominik.suhm@hs-furtwangen.de>
2023-06-29 17:31:57 +0200

    main.py using gtzan as example added comment to requirements.txt

--- main.py
+++ main.py
@@ -2,25 +2,13 @@ from classifySongOnModel import classifySongOnModel
 from makeCSV import makeCSV
 from makeModel import makeModel
 
-makeCSV(partLength: int = 5,
-            genresOfLibrary: str = 'blues classical country disco hiphop jazz metal pop reggae rock',
-            path: str = 'genres',
-            noisy: bool = False,
-            isItMP3: bool = False,
-            nameOfDataCSV: str = 'data.csv')
 
+makeCSV(5, 'blues classical country disco hiphop jazz metal pop reggae rock', 'genres', False, False, 'data.csv')
 
-makeModel(csvFilePath: str = 'data.csv')
+makeModel('data.csv')
 
+t = classifySongOnModel('The Beatles - Hey Jude.mp3', 'model.h5', 'scaler.bin')
 
-t = classifySongOnModel(song: Any,
-                        model: Any,
-                        scaler: Any)
+print(t)
 
-# print(t)
-
-# gtzan + noisy test accuracy: 0.9181286692619324
-# gtzan         test accuracy: 0.826377272605896
-# fma + noisy   test accuracy: 0.8433318138122559
-# fma           test accuracy: 0.6334486603736877
-
+# test_acc:  0.9036144614219666
