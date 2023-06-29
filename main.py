28251bc70d546a0dd8abb1486544bae7ec8bc7ad
guibanen <nahed.guibane@hs-furtwangen.de>
2023-06-29 19:12:11 +0200

    README updated and improved the codes comments

--- main.py
+++ main.py
@@ -2,26 +2,18 @@ from classifySongOnModel import classifySongOnModel
 from makeCSV import makeCSV
 from makeModel import makeModel
 
-makeCSV(partLength: int = 5,
-            genresOfLibrary: str = 'blues classical country disco hiphop jazz metal pop reggae rock',
-            path: str = 'genres',
-            noisy: bool = False,
-            isItMP3: bool = False,
-            nameOfDataCSV: str = 'data.csv')
+# Step 1: Generate the CSV file with audio features
+makeCSV(partLength=5,
+        genresOfLibrary='blues classical country disco hiphop jazz metal pop reggae rock',
+        path='genres',
+        noisy=False,
+        isItMP3=False,
+        nameOfDataCSV='data.csv')
 
+# Step 2: Train the model using the generated CSV file
+makeModel(csvFilePath='data.csv')
 
-makeModel(csvFilePath: str = 'data.csv')
-
-
-t = classifySongOnModel(song: Any,
-                        model: Any,
-                        scaler: Any)
-
-# print(t)
-
-# gtzan + noisy test accuracy: 0.9181286692619324
-# gtzan         test accuracy: 0.826377272605896
-# fma + noisy   test accuracy: 0.8433318138122559
-# fma           test accuracy: 0.6334486603736877
-# jLibrosa Gtzan     :
-# jLibrosa FMA       : 0.6498094797134399
+# Step 3: Classify a song using the trained model
+t = classifySongOnModel("Cascada_-_Everytime_We_Touch_Official_Video.mp3",
+                        "model.h5",
+                        "scaler.bin")
--- makeCSV.py
+++ makeCSV.py
@@ -1,29 +1,26 @@
 import csv
 import os
-
 import librosa
 import numpy as np
 from pydub import AudioSegment
-
 from addNoise import addNoise
 
-
 def generateArrayOfMeanAndSTD(song, filename, genre):
+    # Load the audio signal and calculate the MFCCs
     y, sr = librosa.load(song, mono=True)
     mfcc = librosa.feature.mfcc(y=y, sr=sr)
-    # to_append = f'{filename} {np.mean(chroma_stft)} {np.mean(rmse)} {np.mean(spec_cent)} {np.mean(spec_bw)} {np.mean(rolloff)} {np.mean(zcr)}'
     to_append = f'{filename}'
     for e in mfcc:
+        # Append the MFCC mean and standard deviation to the array
         to_append += f' {np.mean(e)}'
         to_append += f' {np.std(e)}'
-    to_append += f' {genre}'
+    to_append += f' {genre}'  # Append the genre to the array
     return to_append
 
-
 def makeCSV(partLength=5, genresOfLibrary='blues classical country disco hiphop jazz metal pop reggae rock',
-            path='genres', noisy=False, isItMP3 = False, nameOfDataCSV = 'data.csv'):
+            path='genres', noisy=False, isItMP3=False, nameOfDataCSV='data.csv'):
     # Preparing for the CSV file:
-    # Preparing the header row as an array. First the filename than the mfcc values (mean and std) from 1 to 20
+    # Prepare the header row as an array. First, the filename, followed by the MFCC values (mean and std) from 1 to 20
     header = 'filename'
     for i in range(1, 21):
         header += f' mfcc{i}_mean'
@@ -31,18 +28,18 @@ def makeCSV(partLength=5, genresOfLibrary='blues classical country disco hiphop 
     header += ' label'
     header = header.split()
     print("makeCSV Start")
-    # create an empty CSV file and fill the first row with the header array
+    # Create an empty CSV file and fill the first row with the header array
     file = open(nameOfDataCSV, 'w', newline='')
     with file:
         writer = csv.writer(file)
         writer.writerow(header)
 
-    # going through all wav files
+    # Iterate through all wav files
     genres = genresOfLibrary.split()
     for g in genres:
         filenamewhohidden = (filename for filename in os.listdir(f'./{path}/{g}') if not filename.startswith('.'))
         for filename in filenamewhohidden:
-            # preparing variables
+            # Prepare variables
             songname = f'./{path}/{g}/{filename}'
             if isItMP3:
                 FullAudio = AudioSegment.from_mp3(songname)
@@ -50,36 +47,35 @@ def makeCSV(partLength=5, genresOfLibrary='blues classical country disco hiphop 
                 FullAudio = AudioSegment.from_wav(songname)
             lengthOfAudio = FullAudio.duration_seconds
             print("go")
-            # Only if the current song is longer than the length it should have it will splitted
+            # Only split the current song if it is longer than the desired length
             if lengthOfAudio > partLength + 1:
                 splitter = int(lengthOfAudio / partLength)
                 i = 0
                 j = int(0)
-                # The splitter is the number of how many parts it can create of the whole song
+                # The splitter is the number of parts that can be created from the whole song
                 while i < splitter:
-                    # this line is doing the whole trick, it takes out the audio part which is needed
-                    # after that it just export it and it takes the data out of the new wav
+                    # Extract the audio part needed and export it as a new WAV file
                     newAudioPart = FullAudio[j: j + partLength * 1000]
                     newAudioPart.export('tmp.wav', format='wav')
                     to_append = generateArrayOfMeanAndSTD('tmp.wav', filename + str(i), g)
-                    # filling the data of the wav part into the csv
+                    # Fill the data of the WAV part into the CSV
                     file = open(nameOfDataCSV, 'a', newline='')
                     with file:
                         writer = csv.writer(file)
                         writer.writerow(to_append.split())
                     i += 1
                     j = j + partLength * 1000
-                    # creating gausian noised wav file out of the wav part
+                    # Create a Gaussian-noised WAV file out of the WAV part
                     if noisy == True:
                         signal, sr = librosa.load('tmp.wav')
                         addNoise(signal, sr, 'tmp.wav')
                         to_append = generateArrayOfMeanAndSTD('tmp.wav', filename + '_gaussian.' + str(i), g)
-                        # filling the data of the wav part into the csv
+                        # Fill the data of the WAV part into the CSV
                         file = open(nameOfDataCSV, 'a', newline='')
                         with file:
                             writer = csv.writer(file)
                             writer.writerow(to_append.split())
-            # Otherwise the song is shorter than length we want and it creates the value normally
+            # Otherwise, the song is shorter than the desired length and it is processed normally
             else:
                 print("else fall")
                 newAudioPart = FullAudio
@@ -90,11 +86,11 @@ def makeCSV(partLength=5, genresOfLibrary='blues classical country disco hiphop 
                 with file:
                     writer = csv.writer(file)
                     writer.writerow(to_append.split())
-                # creating gausian noised wav file out of the wav part
+                # Create a Gaussian-noised WAV file out of the WAV part
                 signal, sr = librosa.load('tmp.wav')
                 addNoise(signal, sr, 'tmp.wav')
                 to_append = generateArrayOfMeanAndSTD('tmp.wav', filename + '_gaussian', g)
-                # filling the data of the wav part into the csv
+                # Fill the data of the WAV part into the CSV
                 file = open(nameOfDataCSV, 'a', newline='')
                 with file:
                     writer = csv.writer(file)
